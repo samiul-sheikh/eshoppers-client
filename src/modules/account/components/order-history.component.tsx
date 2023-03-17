56b4e9992572789b908606667bda4cf0b38b0031
samiul-sheikh <sheikh.samiul1971@gmail.com>
2023-03-17 23:28:01 +0600

    design order history page

--- src/modules/account/components/order-history.component.tsx
+++ src/modules/account/components/order-history.component.tsx
@@ -14,59 +14,74 @@ export const OrderHistoryComponent: React.FC<OrderHistoryComponentProps> = (
       </div>
       <div className="col-span-9 space-y-4 mt-6 lg:mt-0">
         <div className="flex items-center md:justify-between gap-4 md:gap-6 p-4 border border-gray-200 rounded flex-wrap md:flex-nowrap">
-          <div className="w-32 flex-shrink-0">
-            <img src={product1} className="w-full" alt="product1" />
+          <div className="w-10 flex-shrink-0">
+            <img src={product1} className="w-full" alt="wishlistProduct1" />
           </div>
-          <div className="md:w-1/3 w-full">
-            <h2 className="text-gray-800 mb-3 xl:text-xl textl-lg font-medium uppercase">
-              MENS JACKET
-            </h2>
-            <p className="text-primary font-semibold">$300.00</p>
-            <p className="text-gray-500">Size: M</p>
+          <div>
+            <h2 className="text-gray-600 font-semibold">Mens Jacket</h2>
           </div>
-          <div className="flex border border-gray-300 text-gray-600 divide-x divide-gray-300">
-            <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">
-              -
-            </div>
-            <div className="h-8 w-10 flex items-center justify-center">4</div>
-            <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">
-              +
-            </div>
+          <div>
+            <p className="text-gray-600 font-semibold">Order Number</p>
+            <p className="text-gray-500">1234567</p>
           </div>
-          <div className="ml-auto md:ml-0">
-            <p className="text-primary text-lg font-semibold">$1200.00</p>
+          <div>
+            <p className="text-gray-600 font-semibold">Purchased</p>
+            <p className="text-gray-500">14 Nov 22</p>
           </div>
-          <div className="text-gray-600 hover:text-primary cursor-pointer">
-            <i className="fas fa-trash"></i>
+          <div>
+            <p className="text-gray-600 font-semibold">Qut</p>
+            <p className="text-gray-500">x3</p>
           </div>
+          <div>
+            <p className="text-gray-600 font-semibold">Total</p>
+            <p className="text-gray-500">$300</p>
+          </div>
+          <div>
+            <p className="text-gray-600 font-semibold">Status</p>
+            <p className="text-green-500">Delivered</p>
+          </div>
+          <a
+            href="/order-details"
+            className="px-6 py-2 text-center text-sm text-primary
+            bg-white border border-primary rounded hover:bg-primary hover:text-white transition font-roboto font-medium"
+          >
+            View Order
+          </a>
         </div>
         <div className="flex items-center md:justify-between gap-4 md:gap-6 p-4 border border-gray-200 rounded flex-wrap md:flex-nowrap">
-          <div className="w-32 flex-shrink-0">
-            <img src={product1} className="w-full" alt="product1" />
+          <div className="w-10 flex-shrink-0">
+            <img src={product1} className="w-full" alt="wishlistProduct1" />
           </div>
-          <div className="md:w-1/3 w-full">
-            <h2 className="text-gray-800 mb-3 xl:text-xl textl-lg font-medium uppercase">
-              MENS JACKET
-            </h2>
-            <p className="text-primary font-semibold">$300.00</p>
-            <p className="text-gray-500">Size: M</p>
+          <div>
+            <h2 className="text-gray-600 font-semibold">Mens Jacket</h2>
           </div>
-
-          <div className="flex border border-gray-300 text-gray-600 divide-x divide-gray-300">
-            <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">
-              -
-            </div>
-            <div className="h-8 w-10 flex items-center justify-center">4</div>
-            <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">
-              +
-            </div>
+          <div>
+            <p className="text-gray-600 font-semibold">Order Number</p>
+            <p className="text-gray-500">1234567</p>
           </div>
-          <div className="ml-auto md:ml-0">
-            <p className="text-primary text-lg font-semibold">$1200.00</p>
+          <div>
+            <p className="text-gray-600 font-semibold">Purchased</p>
+            <p className="text-gray-500">14 Nov 22</p>
           </div>
-          <div className="text-gray-600 hover:text-primary cursor-pointer">
-            <i className="fas fa-trash"></i>
+          <div>
+            <p className="text-gray-600 font-semibold">Qut</p>
+            <p className="text-gray-500">x3</p>
           </div>
+          <div>
+            <p className="text-gray-600 font-semibold">Total</p>
+            <p className="text-gray-500">$300</p>
+          </div>
+          <div>
+            <p className="text-gray-600 font-semibold">Status</p>
+            <p className="text-red-500">Cancelled</p>
+          </div>
+          <a
+            href="/order-details"
+            className="px-6 py-2 text-center text-sm text-primary
+            bg-white border border-primary rounded hover:bg-primary hover:text-white transition font-roboto font-medium"
+          >
+            View Order
+          </a>
         </div>
       </div>
     </div>
